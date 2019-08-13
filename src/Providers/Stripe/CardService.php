c16cafe4328607a6562c155f21e62d20bf78c946
adteulade <adteulade@gmail.com>
2019-08-13 15:19:04 +0100

    finishing the card service.

--- src/Providers/Stripe/CardService.php
+++ src/Providers/Stripe/CardService.php
@@ -11,19 +11,26 @@ use Stripe\SetupIntent;
 class CardService extends Base implements CardServiceInterface
 {
     /**
-     * Tries create a setup card request using the Stripe SDK.
+     * Tries create a card using the Stripe SDK.
+     *
+     * @param string|null $token
      *
      * @return Card
      *
      * @throws Exception
      * @throws StripeException
      */
-    public function create(): Card
+    public function create($token = null): Card
     {
         try {
-            $response = SetupIntent::create([
-                'usage' => 'on_session',
-            ]);
+            if (is_null($token)) {
+                $response = SetupIntent::create([
+                    'usage' => 'on_session',
+                    'payment_method_types' => ['card'],
+                ]);
+            } else {
+                $response = SetupIntent::retrieve($token);
+            }
         } catch (\Exception $exception) {
             throw new Exception($exception);
         }
